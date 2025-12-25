7101e6898ed15214a109766af05c111fcf57f2fc
Le_Quang_Huan <huanbh2000@gmail.com>
2025-12-25 13:48:34 +0700

    BTH

--- BTH3_LCD_I2C/bai3.2/bth3_2/bth3_2.ino
+++ BTH3_LCD_I2C/bai3.2/bth3_2/bth3_2.ino
@@ -2,232 +2,312 @@
 #include <RTClib.h>
 #include <LiquidCrystal_I2C.h>
 
-#define BTN_MODE 0
-#define BTN_UP   1
-#define BTN_DOWN 2
-#define BTN_SET  3
-#define LED_ALARM 6   // thay cho buzzer
+#define PIN_BTN_MODE 2
+#define PIN_BTN_UP 3
+#define PIN_BTN_DOWN 4
+#define PIN_BTN_SET 5
+#define PIN_LED 7
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 RTC_DS1307 rtc;
 
 enum ModeState {
   MODE_VIEW_TIME,
   MODE_SET_ALARM_H,
-  MODE_SET_ALARM_M
+  MODE_SET_ALARM_M,
+  MODE_SET_TIME_H,
+  MODE_SET_TIME_M
 };
 
-ModeState mode = MODE_VIEW_TIME;
+ModeState currentMode = MODE_VIEW_TIME;
 
-int alarmHour = 6, alarmMinute = 0;
-bool alarmOn = false;
-bool isRinging = false;
+int alarmH = 0;
+int alarmM = 0;
+bool isAlarmOn = 0;
+bool isRing = false;
 
-int tempHour, tempMinute;
+int tempH, tempM;
 
-unsigned long pressStart = 0;
-bool holdDone = false;
+unsigned long modePressTime = 0;
+bool modeButtonHold = false;
 
-void setup() {
-  pinMode(BTN_MODE, INPUT_PULLUP);
-  pinMode(BTN_UP, INPUT_PULLUP);
-  pinMode(BTN_DOWN, INPUT_PULLUP);
-  pinMode(BTN_SET, INPUT_PULLUP);
-  pinMode(LED_ALARM, OUTPUT);
-
-  lcd.init();
-  lcd.backlight();
-
-  if (!rtc.begin()) {
-    lcd.print("Loi RTC!");
-    while (1);
-  }
-  if (!rtc.isrunning()) rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-  lcd.clear();
-}
-
-void loop() {
-  handleButtons();
-
-  if (isRinging) {
-    ringAlarm();
-    return;
-  }
-
-  switch (mode) {
-    case MODE_VIEW_TIME:
-      displayRealTime();
-      checkAlarmTrigger();
-      break;
-
-    case MODE_SET_ALARM_H:
-    case MODE_SET_ALARM_M:
-      displaySetAlarm();
-      break;
-  }
-
-  delay(80);
-}
-
-void handleButtons() {
-  int modeState = digitalRead(BTN_MODE);
-
-  // --- Giữ nút MODE 2 giây để bật/tắt báo thức ---
-  if (modeState == LOW) {
-    if (pressStart == 0) pressStart = millis();
-    if (millis() - pressStart > 2000 && !holdDone && mode == MODE_VIEW_TIME) {
-      alarmOn = !alarmOn;
-      holdDone = true;
+void Buttons()
+{
+  if (digitalRead(PIN_BTN_MODE) == LOW)
+  {
+    if (modePressTime == 0) modePressTime = millis();
+    if (millis() - modePressTime > 2000 && !modeButtonHold && currentMode == MODE_VIEW_TIME)
+    {
+      isAlarmOn = !isAlarmOn; 
+      modeButtonHold = true; 
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print(alarmOn ? "Bao thuc: ON " : "Bao thuc: OFF");
+      lcd.print(isAlarmOn ? "Bat bao thuc    " : "Tat bao thuc    ");
       delay(1000);
       lcd.clear();
     }
-  } else {
-    if (pressStart > 0) {
-      if (!holdDone && millis() - pressStart < 1000) {
-        // Nhấn ngắn: chuyển chế độ hoặc tắt chuông
-        if (isRinging) stopAlarm();
-        else changeMode();
+  }
+  else 
+  {
+    if (modePressTime > 0)
+    {
+      if (!modeButtonHold && millis() - modePressTime < 1000)
+      {
+        if (isRing)
+        {
+          stopAlarm(); 
+        }
+        else
+        {
+          changeMode();
+        }
       }
-      pressStart = 0;
-      holdDone = false;
+      modePressTime = 0;
+      modeButtonHold = false;
     }
   }
 
-  // Nút UP / DOWN / SET
-  if (digitalRead(BTN_UP) == LOW) {
-    handleUp();
+  if (digitalRead(PIN_BTN_UP) == LOW)
+  {
+    UpTime();
     delay(200);
   }
-  if (digitalRead(BTN_DOWN) == LOW) {
-    handleDown();
+
+  if (digitalRead(PIN_BTN_DOWN) == LOW)
+  {
+    DownTime();
     delay(200);
   }
-  if (digitalRead(BTN_SET) == LOW) {
-    handleSet();
+
+  if (digitalRead(PIN_BTN_SET) == LOW)
+  {
+    SetTimeMode();
     delay(300);
   }
 }
 
-// --- Chuyển chế độ ---
-void changeMode() {
+void changeMode()
+{
   lcd.clear();
-  if (mode == MODE_VIEW_TIME) {
-    mode = MODE_SET_ALARM_H;
-    tempHour = alarmHour;
-    tempMinute = alarmMinute;
-  } else if (mode == MODE_SET_ALARM_H) {
-    mode = MODE_SET_ALARM_M;
-  } else {
-    mode = MODE_VIEW_TIME;
+  switch (currentMode)
+  {
+    case MODE_VIEW_TIME:
+      currentMode = MODE_SET_ALARM_H;
+      tempH = alarmH;
+      tempM = alarmM;
+      break;
+    case MODE_SET_ALARM_H: 
+    case MODE_SET_ALARM_M:
+      currentMode = MODE_SET_TIME_H;
+      {
+        DateTime now = rtc.now();
+        tempH = now.hour();
+        tempM = now.minute();
+      }
+      break;
+    default:
+      currentMode = MODE_VIEW_TIME;
+      break;
   }
 }
 
-// --- Xử lý nút tăng/giảm ---
-void handleUp() {
-  if (mode == MODE_SET_ALARM_H) {
-    tempHour = (tempHour + 1) % 24;
-  } else if (mode == MODE_SET_ALARM_M) {
-    tempMinute = (tempMinute + 1) % 60;
+void UpTime()
+{
+  if (currentMode == MODE_SET_ALARM_H || currentMode == MODE_SET_TIME_H)
+  {
+    tempH++;
+    if (tempH > 23) tempH = 0;
+  }
+  else if (currentMode == MODE_SET_ALARM_M || currentMode == MODE_SET_TIME_M)
+  {
+    tempM++;
+    if (tempM > 59) tempM = 0;
   }
 }
 
-void handleDown() {
-  if (mode == MODE_SET_ALARM_H) {
-    tempHour = (tempHour + 23) % 24;
-  } else if (mode == MODE_SET_ALARM_M) {
-    tempMinute = (tempMinute + 59) % 60;
+void DownTime()
+{
+  if (currentMode == MODE_SET_ALARM_H || currentMode == MODE_SET_TIME_H)
+  {
+    tempH--;
+    if (tempH < 0) tempH = 23;
+  }
+  else if (currentMode == MODE_SET_ALARM_M || currentMode == MODE_SET_TIME_M)
+  {
+    tempM--;
+    if (tempM < 0) tempM = 59;
   }
 }
 
-// --- Nút SET ---
-void handleSet() {
-  if (mode == MODE_SET_ALARM_H) {
-    mode = MODE_SET_ALARM_M;
-  } else if (mode == MODE_SET_ALARM_M) {
-    alarmHour = tempHour;
-    alarmMinute = tempMinute;
-    alarmOn = true;
-    mode = MODE_VIEW_TIME;
-    lcd.clear();
-    lcd.print("Da luu bao thuc!");
-    delay(1000);
-    lcd.clear();
-  }
-}
-
-// --- Hiển thị thời gian thực ---
-void displayRealTime() {
+void SetTimeMode()
+{
   DateTime now = rtc.now();
+  switch (currentMode)
+  {
+    case MODE_SET_ALARM_H:
+      currentMode = MODE_SET_ALARM_M; 
+      break;
+    case MODE_SET_ALARM_M:
+      alarmH = tempH; 
+      alarmM = tempM;
+      isAlarmOn = true; 
+      currentMode = MODE_VIEW_TIME;
+      lcd.clear();
+      lcd.print("Da dat bao thuc!");
+      delay(1000);
+      lcd.clear();
+      break;
+    case MODE_SET_TIME_H:
+      currentMode = MODE_SET_TIME_M; 
+      break;
+    case MODE_SET_TIME_M:
+      rtc.adjust(DateTime(now.year(), now.month(), now.day(), tempH, tempM, 0));
+      currentMode = MODE_VIEW_TIME;
+      lcd.clear();
+      lcd.print("Da dat lai gio! ");
+      delay(1000);
+      lcd.clear();
+      break;
+  }
+}
 
+void printDigits(int digits)
+{
+  if (digits < 10) lcd.print('0');
+  lcd.print(digits);
+}
+
+void displayRealTime()
+{
+  DateTime now = rtc.now();
   lcd.setCursor(0, 0);
   lcd.print("Time: ");
-  if (now.hour() < 10) lcd.print('0');
-  lcd.print(now.hour());
-  lcd.print(':');
-  if (now.minute() < 10) lcd.print('0');
-  lcd.print(now.minute());
-  lcd.print(':');
-  if (now.second() < 10) lcd.print('0');
-  lcd.print(now.second());
+  printDigits(now.hour()); lcd.print(":");
+  printDigits(now.minute()); lcd.print(":");
+  printDigits(now.second());
 
   lcd.setCursor(0, 1);
-  lcd.print(alarmOn ? "ON " : "OFF");
-  lcd.print(" Hen:");
-  if (alarmHour < 10) lcd.print('0');
-  lcd.print(alarmHour);
-  lcd.print(':');
-  if (alarmMinute < 10) lcd.print('0');
-  lcd.print(alarmMinute);
-}
-
-// --- Hiển thị khi cài báo thức ---
-void displaySetAlarm() {
-  lcd.setCursor(0, 0);
-  lcd.print("CAI BAO THUC");
-
-  lcd.setCursor(0, 1);
-  if (mode == MODE_SET_ALARM_H) lcd.print(">");
-  else lcd.print(" ");
-  if (tempHour < 10) lcd.print('0');
-  lcd.print(tempHour);
-  lcd.print(':');
-
-  if (mode == MODE_SET_ALARM_M) lcd.print(">");
-  else lcd.print(" ");
-  if (tempMinute < 10) lcd.print('0');
-  lcd.print(tempMinute);
-}
-
-// --- Kiểm tra đến giờ báo thức ---
-void checkAlarmTrigger() {
-  if (!alarmOn) return;
-
-  DateTime now = rtc.now();
-  if (now.hour() == alarmHour && now.minute() == alarmMinute && now.second() == 0) {
-    isRinging = true;
+  lcd.print("Alarm: ");
+  if (isAlarmOn) lcd.print("ON ");
+  else lcd.print("OFF");
+  if (isAlarmOn)
+  {
+    printDigits(alarmH); lcd.print(":");
+    printDigits(alarmM);
   }
 }
 
-// --- Báo thức: LED chớp ---
-void ringAlarm() {
+void displaySetAlarm()
+{
   lcd.setCursor(0, 0);
-  lcd.print("DEN GIO ROI!!!  ");
+  lcd.print("Dat bao thuc");
   lcd.setCursor(0, 1);
-  lcd.print("Bam MODE de tat");
-
-  digitalWrite(LED_ALARM, HIGH);
-  delay(200);
-  digitalWrite(LED_ALARM, LOW);
-  delay(200);
+  
+  if (currentMode == MODE_SET_ALARM_H) lcd.print(">"); 
+  else lcd.print(" ");
+  printDigits(tempH);
+  lcd.print(":");
+  
+  if (currentMode == MODE_SET_ALARM_M) lcd.print(">"); 
+  else lcd.print(" ");
+  printDigits(tempM);
 }
 
-// --- Tắt báo thức ---
-void stopAlarm() {
-  isRinging = false;
-  digitalWrite(LED_ALARM, LOW);
+void displaySetTime()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Sua thoi gian");
+  lcd.setCursor(0, 1);
+  
+  if (currentMode == MODE_SET_TIME_H) lcd.print(">");
+  else lcd.print(" ");
+  printDigits(tempH);
+  lcd.print(":");
+  
+  if (currentMode == MODE_SET_TIME_M) lcd.print(">");
+  else lcd.print(" ");
+  printDigits(tempM);
+}
+
+void checkAlarm()
+{
+  if (!isAlarmOn) return;
+  
+  DateTime now = rtc.now();
+  if (now.hour() == alarmH && now.minute() == alarmM && now.second() == 0)
+  {
+    isRing = true;
+  }
+} 
+
+void ringAlarm()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Tut Tut Tut!!!  ");
+  lcd.setCursor(0, 1);
+  lcd.print("Bam MODE de tat ");
+  
+  digitalWrite(PIN_LED, HIGH);
+  delay(100);
+  digitalWrite(PIN_LED, LOW);
+  delay(100);
+}
+
+void stopAlarm()
+{
+  isRing = false;
+  digitalWrite(PIN_LED, HIGH);
   lcd.clear();
+}
+
+void setup() {
+  pinMode(PIN_BTN_MODE, INPUT_PULLUP);
+  pinMode(PIN_BTN_UP, INPUT_PULLUP);
+  pinMode(PIN_BTN_DOWN, INPUT_PULLUP);
+  pinMode(PIN_BTN_SET, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT);
+  
+  digitalWrite(PIN_LED, 1);
+
+  lcd.init();
+  lcd.backlight();
+
+  if (!rtc.begin())
+  {
+    lcd.print("Loi RTC!");
+    while (1);
+  }
+
+  if (!rtc.isrunning())
+  {
+    // Nếu RTC chưa chạy, set giờ theo thời gian biên dịch code
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop() {
+  Buttons();
+  
+  if (isRing)
+  {
+    ringAlarm();
+  }
+  else
+  {
+    switch (currentMode) {
+      case MODE_VIEW_TIME:
+        displayRealTime();
+        checkAlarm();
+        break;
+      case MODE_SET_ALARM_H:
+      case MODE_SET_ALARM_M:
+        displaySetAlarm();
+        break;
+      case MODE_SET_TIME_H:
+      case MODE_SET_TIME_M:
+        displaySetTime();
+        break;
+    }
+  }
+  delay(50);
 }
